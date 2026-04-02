eabe63ad0b365a50e896a022f05580346f6f34b9
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2026-04-02 22:57:42 +0100

    Delete loading.tsx

--- loading.tsx
+++
@@ -1,9 +0,0 @@
-/**import Image from "next/image"
-
-export default function Loading() {
-    return(
-        <div className="flex justify-center items-center h-screen w-full">
-            <Image src="/icons8-loading.gif" alt="Loading..." />
-        </div>
-    )
-}**/
